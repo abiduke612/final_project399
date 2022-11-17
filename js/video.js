b261ac58b33dcf775ae2d3f79b4bfd924170177d
abiduke612 <92966020+abiduke612@users.noreply.github.com>
2022-11-17 14:52:26 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,11 @@
+// var video;
+
+// window.addEventListener("load", function() {
+// 	video = document.querySelector("#earth");
+//     video.play();
+// 	console.log("Good job opening the window");
+// 	video.autoplay = false;
+// 	console.log("Auto play is set to true");
+// 	video.loop = true;
+// 	console.log("Loop is set to true");
+// });
