b460f8ec4d9fe47e4cf51f14a1e6bbb5873c0e1b
abiduke612 <92966020+abiduke612@users.noreply.github.com>
2022-12-06 21:41:34 -0500

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -83,7 +83,7 @@
     const submitButton = document.getElementById('submit');
     const myQuestions = [
         {
-            question: "Americans use about ____ of the world's energy each year",
+            question: "1. Americans use about ____ of the world's energy each year",
             answers: {
               a: "10%",
               b: "3%",
@@ -93,7 +93,7 @@
             correctAnswer: "d"
         },
         {
-            question: "How many gallons of water does one pair of jeans take to produce? (Hint: think about the water needed to grow cotton, etc.)",
+            question: "2. How many gallons of water does one pair of jeans take to produce? (Hint: think about the water needed to grow cotton, etc.)",
             answers: {
             a: "180",
             b: "1800",
@@ -103,7 +103,7 @@
             correctAnswer: "b"
         },
         {
-            question: "What is NOT something you as an individual should do to help sustainability?",
+            question: "3. What is NOT something you as an individual should do to help sustainability?",
             answers: {
             a: "Recycle",
             b: "Drive alone to work everyday",
@@ -111,6 +111,76 @@
             d: "Turn of lights when you're not using them"
             },
             correctAnswer: "b"
+        },
+        {
+          question: "4. What is NOT a type of renewable energy?",
+          answers: {
+          a: "Gas",
+          b: "Solar",
+          c: "Wind",
+          d: "Geothermal"
+          },
+          correctAnswer: "a"
+        },
+        {
+          question: "5. How many years does it take for a plastic water bottle to decompose completely?",
+          answers: {
+          a: "10 years",
+          b: "250 years",
+          c: "450 years",
+          d: "1000 years"
+          },
+          correctAnswer: "c"
+        },
+        {
+          question: "6. When you turn on an incandescent light bulb, how much of the energy is wasted (not turned into light)?",
+          answers: {
+          a: "99",
+          b: "50",
+          c: "90",
+          d: "10"
+          },
+          correctAnswer: "c"
+        },
+        {
+          question: "7. What was NOT one of the 3 important sustainability laws passed in 1970?",
+          answers: {
+          a: "Clean Air Act",
+          b: "Clean Water Act",
+          c: "Clean Homes Act",
+          d: "Toxic Substances Control Act"
+          },
+          correctAnswer: "c"
+        },
+        {
+          question: "8. About how many gallons of water does the average person use per year?",
+          answers: {
+          a: "5,000",
+          b: "30,000",
+          c: "1,000",
+          d: "95,000"
+          },
+          correctAnswer: "b"
+        },
+        {
+          question: "9. What animal contributes the most to climate change?",
+          answers: {
+          a: "Pigs",
+          b: "Dogs",
+          c: "Chickens",
+          d: "Cattle"
+          },
+          correctAnswer: "d"
+        },
+        {
+          question: "10. What is the most sustainable country in the world?",
+          answers: {
+          a: "USA",
+          b: "Iceland",
+          c: "Norway",
+          d: "Finland"
+          },
+          correctAnswer: "d"
         }
     ];
   
