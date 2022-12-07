59fef5bcff637bb4b4301f75c90c7b7d3c96a62b
abiduke612 <92966020+abiduke612@users.noreply.github.com>
2022-12-06 19:34:50 -0500

    Add files via upload

---
+++ js/ind.js
@@ -0,0 +1,53 @@
+// document.querySelector("#beef").addEventListener("click", function() {
+
+//     event.target.style.color = 'black';
+
+// })
+
+document.querySelector("#beef").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#bike").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#clothes").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#clothesline").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#compost").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#groceries").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#light").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#notes").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#recycle").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#shower").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#tupperware").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
+
+document.querySelector("#xeriscape").addEventListener("click", function() {
+    event.target.style.color = 'black';
+})
