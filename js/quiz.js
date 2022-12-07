9f287e097a7beb09663ec2365e0dac741ea9e00c
abiduke612 <92966020+abiduke612@users.noreply.github.com>
2022-12-06 20:27:26 -0500

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -1,107 +1,122 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-
-const myQuestions = [
-    {
-      question: "Americans use about ____ of the world's energy each year",
-      answers: {
-        a: "10%",
-        b: "3%",
-        c: "19%", 
-        d: "25%"
-      },
-      correctAnswer: "d"
-    },
-    {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
-    }
-  ];
-
-
-
-
-
-function buildQuiz(){
-
-    const output = [];
-
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
-
-        for(letter in currentQuestion.answers) {
+(function(){
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
+  
+      // for each question...
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
+  
+            // ...add an HTML radio button
             answers.push(
-                `<label>
-                    <input type="radio" name="question${questionNumber}" value="${letter}">
-                    ${letter}:
-                    ${currentQuestion.answers[letter]}
-                </label>`
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
             );
-        }
-
-        output.push(
+          }
+  
+          // add this question and its answers to the output
+          output.push(
             `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div> `
-        );
+            <div class="answers"> ${answers.join('')} </div>`
+          );
         }
-    );
-    quizContainer.innerHTML = output.join('');
-}
-
-function showResults(){
-
-    // gather answer containers from our quiz
-  const answerContainers = quizContainer.querySelectorAll('.answers');
-
-  // keep track of user's answers
-  let numCorrect = 0;
-
-  // for each question...
-  myQuestions.forEach( (currentQuestion, questionNumber) => {
-
-    // find selected answer
-    const answerContainer = answerContainers[questionNumber];
-    const selector = `input[name=question${questionNumber}]:checked`;
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-    // if answer is correct
-    if(userAnswer === currentQuestion.correctAnswer){
-      // add to the number of correct answers
-      numCorrect++;
-
-      // color the answers green
-      answerContainers[questionNumber].style.color = 'lightgreen';
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
     }
-    // if answer is wrong or blank
-    else{
-      // color the answers red
-      answerContainers[questionNumber].style.color = 'red';
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        answerContainers[questionNumber].innerHTML = answerContainers[questionNumber].innerHTML.replace('<br> Incorrect!', "");
+        answerContainers[questionNumber].innerHTML = answerContainers[questionNumber].innerHTML.replace('<br> Correct!', "");
+  
+        // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'green';
+
+          answerContainers[questionNumber].innerHTML += '<br> Correct!';
+          
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[questionNumber].style.color = '#BD0000';
+
+          answerContainers[questionNumber].innerHTML += '<br> Incorrect!';
+        }
+      });
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = `<br> &nbsp &nbsp &nbsp ${numCorrect} out of ${myQuestions.length}`;
     }
-  });
-
-  // show number of correct answers out of total
-  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-
-}
-
-// display quiz right away
-buildQuiz();
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);
+  
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+        {
+            question: "Americans use about ____ of the world's energy each year",
+            answers: {
+              a: "10%",
+              b: "3%",
+              c: "19%", 
+              d: "25%"
+            },
+            correctAnswer: "d"
+        },
+        {
+            question: "How many gallons of water does one pair of jeans take to produce? (Hint: think about the water needed to grow cotton, etc.)",
+            answers: {
+            a: "180",
+            b: "1800",
+            c: "18,000",
+            d: "18"
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "What is NOT something you as an individual should do to help sustainability?",
+            answers: {
+            a: "Recycle",
+            b: "Drive alone to work everyday",
+            c: "Cut beef out of your diet",
+            d: "Turn of lights when you're not using them"
+            },
+            correctAnswer: "b"
+        }
+    ];
+  
+    // Kick things off
+    buildQuiz();
+  
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+  })();
